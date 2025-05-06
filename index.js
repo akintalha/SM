f040d0160e56a08f823cad93bdafcfed00425797
Akın <80766655+akintalha@users.noreply.github.com>
2025-05-07 02:59:03 +0300

    çok güzel kod

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
 const device = require('express-device');
+const nodemailer = require('nodemailer');
 var cookieParser = require('cookie-parser');
-
-
 
 const app = express();
 app.set('views', path.join(__dirname + '/views/'));
-app.engine(settings.hbsextension, exphbs({ helpers: hbshelper, extname: settings.hbsextension, defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/Layouts/', partialsDir: __dirname + "/views/Partials" }));
+app.engine(settings.hbsextension, exphbs({
+    helpers: hbshelper,
+    extname: settings.hbsextension,
+    defaultLayout: 'mainLayout',
+    layoutsDir: __dirname + '/views/Layouts/',
+    partialsDir: __dirname + "/views/Partials"
+}));
 app.set('view engine', settings.hbsextension);
 app.use('/assets', express.static(__dirname + '/assets'));
 
@@ -19,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(device.capture());
 app.use(cookieParser());
+
 function renderParams(data, user, gridprop, layout = "mainLayout") {
     var renderedOBJ = {};
     if (data) {
@@ -39,7 +45,7 @@ function renderParams(data, user, gridprop, layout = "mainLayout") {
 }
 
 app.get("/", function (req, res) {
-        res.render("index", renderParams(null, null, null));
+    res.render("index", renderParams(null, null, null));
 });
 app.get("/content", function (req, res) {
     res.render("content", renderParams(null, null, null));
@@ -60,9 +66,68 @@ app.get("/privacystatement", function (req, res) {
     res.render("privacystatement", renderParams(null, null, null));
 });
 
+app.post("/submit-form", async function (req, res) {
+    const { fullname,
+        email,
+        phone,
+        prefs,
+        datetime,
+        datetime2,
+        duration,
+        topic,
+        concerns,
+        payment,
+        agreement,
+        anything } = req.body;
+
+    // E-posta gönderme ayarları
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.hostinger.com', // SMTP sunucusu
+        port: 465,  // SSL için 465, TLS için 587
+        secure: true, // SSL için true, TLS için false
+        auth: {
+            user: 'admin@smbeautybroker.co.uk',
+            pass: 'Smbeauty123.'
+        }
+    });
+
+    const mailOptions = {
+        from: 'admin@smbeautybroker.co.uk',
+        to: 'admin@smbeautybroker.co.uk',
+        subject: 'Booking Form Submission',
+        html: `
+            <h3>Booking Form</h3>
+        <p><strong>Full Name:</strong> ${fullname}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Preferred Contact Methods:</strong> ${prefs}</p>
+        <hr>
+         <p><strong>Consultation Date & Time (Option 1):</strong> ${datetime}</p>
+        <p><strong>Consultation Date & Time (Option 2):</strong> ${datetime2}</p>
+        <p><strong>Consultation Duration:</strong> ${duration}</p>
+        <p><strong>Topic of Consultation:</strong> ${topic}</p>
+        <hr>
+        <p><strong>Medical Concerns:</strong> ${concerns}</p>
+        <hr>
+        <p><strong>Preferred Payment Method:</strong> ${payment}</p>
+        <hr>
+        <p><strong>Privacy Agreement:</strong> ${agreement}</p>
+        <hr>
+        <p><strong>Additional Comments:</strong> ${anything}</p>
+        `
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send("E-posta başarıyla gönderildi.");
+    } catch (error) {
+        console.error("Mail gönderim hatası:", error);
+        res.status(500).send("E-posta gönderilemedi.");
+    }
+});
+
 app.listen(settings._Port, function (err, data) {
     if (!err) {
         console.log("server started in " + settings._Port + " port");
     }
-}),
-3
+});
